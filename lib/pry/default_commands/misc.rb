7a19533aefe07417df8ff707aad72ab13abb153a
Kyrylo Silin <kyrylosilin@gmail.com>
2012-08-08 08:11:46 +0300

    Implement `list` command for remote plugins
    Refactor old code for local list of plugins. Add new code for ability to
    show remote list of plugins. The "plugin" command is now able:
    
      1. To show list of local plugins (old feature).
    
           pry(main)> plugin list
    
      2. To show list of remote plugins (repeated calls give results from
         the cache).
    
           pry(main)> plugin list -r
    
      3. To show list of remote plugins forcefully (makes another request to
         RubyGems API and refreshes the cache).
    
           pry(main)> plugin list -rf
    
    Signed-off-by: Kyrylo Silin <kyrylosilin@gmail.com>

--- lib/pry/default_commands/misc.rb
+++ lib/pry/default_commands/misc.rb
@@ -41,14 +41,36 @@ class Pry
           Usage: plugin
         BANNER
 
-        def options(opt)
-          opt.on :l, :list, "List all installed plugins", :argument => false
+        def sub_commands(cmd)
+          cmd.on :list do
+            on :r, "remote", "Show the list of all available plugins"
+            on :f, "force",  "Refresh cached list of remote plugins"
+
+            add_callback(:empty) {
+              PluginManager.show_installed_plugins(Pry.plugins)
+            }
+          end
         end
 
         def process
-          if opts.l?
-            PluginManager.show_installed_plugins(Pry.plugins, output)
-          end
+          show_remote = opts[:list].present?(:remote)
+          force       = show_remote && opts[:list].present?(:force)
+
+          show_remote_plugins(force) if show_remote
+        end
+
+        private
+
+        # Displays the list of remote plugins. Fetches the list of remote Pry
+        # plugins if #{Pry.remote_plugins} hash is empty.
+        #
+        # @param [Boolean] force The flag, which specifies whether the list of
+        #   remote plugins should be refreshed or not. If not, then it recalls
+        #   {PluginManager#find_remote_plugins} method.
+        # @return [void]
+        def show_remote_plugins(force = false)
+          Pry.locate_plugins(:remote) if Pry.remote_plugins.empty? || force
+          PluginManager.show_remote_plugins(Pry.remote_plugins)
         end
 
       end
